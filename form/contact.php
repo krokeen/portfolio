6a406c51c412365eca868d82fa32d10ebe315f8a
Mahmoud Galal <79154088+krokeen@users.noreply.github.com>
2021-05-27 13:16:37 +0200

    Update contact.php

--- form/contact.php
+++ form/contact.php
@@ -1,29 +1,15 @@
 <?php
 
-// an email address that will be in the From field of the email.
-$from = 'hi@kroking.me';
-
-// an email address that will receive the email with the output of the form
+$from = '';
 $sendTo = 'hi@kroking.me';
-
-// subject of the email
 $subject = 'New message from contact form';
-
-// form field names and their translations.
-// array variable name => Text to appear in the email
 $fields = array('InputName' => 'Name', 'email' => 'InputEmail', 'InputMessage' => 'Message'); 
 
-// message that will be displayed when everything is OK :)
 $okMessage = 'Your message successfully submitted. Thank you, I will get back to you soon!';
 
-// If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-/*
- *  LET'S DO THE SENDING
- */
 
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 try
@@ -40,14 +26,12 @@ try
         }
     }
 
-    // All the neccessary headers for the email.
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
         'From: ' . $from,
         'Reply-To: ' . $from,
         'Return-Path: ' . $from,
     );
-    
-    // Send email
+
     mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
@@ -58,7 +42,6 @@ catch (\Exception $e)
 }
 
 
-// if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
 
@@ -66,7 +49,6 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 
     echo $encoded;
 }
-// else just display the message
 else {
     echo $responseArray['message'];
 }
